5e9bc1bc3d146d8576e9adcd3f9f7448b33db717
Arjay Bayona <arjayby1@gmail.com>
2021-03-22 01:05:21 +0800

    ⚡ Add before create hook

--- src/services/members/members.hooks.ts
+++ src/services/members/members.hooks.ts
@@ -1,4 +1,5 @@
 import * as authentication from '@feathersjs/authentication';
+import membersModifyContextData from '../../hooks/members-modify-context-data';
 // Don't remove this comment. It's needed to format import lines nicely.
 
 const { authenticate } = authentication.hooks;
@@ -8,7 +9,7 @@ export default {
     all: [ authenticate('jwt') ],
     find: [],
     get: [],
-    create: [],
+    create: [membersModifyContextData()],
     update: [],
     patch: [],
     remove: []
