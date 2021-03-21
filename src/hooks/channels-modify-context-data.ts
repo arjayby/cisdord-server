0b03d89c779ff5d59c5544d75838b0bd01d44aef
Arjay Bayona <arjayby1@gmail.com>
2021-03-21 22:47:11 +0800

    ⚡ Add before create hook

---
+++ src/hooks/channels-modify-context-data.ts
@@ -0,0 +1,15 @@
+// Use this hook to manipulate incoming or outgoing data.
+// For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
+import { Hook, HookContext } from '@feathersjs/feathers';
+import { nanoid } from 'nanoid';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default (options = {}): Hook => {
+  return async (context: HookContext): Promise<HookContext> => {
+    
+    context.data.shortId = nanoid(10);
+    context.data.userId = context.params?.user?.id;
+
+    return context;
+  };
+};
--- src/services/channels/channels.hooks.ts
+++ src/services/channels/channels.hooks.ts
@@ -1,4 +1,5 @@
 import * as authentication from '@feathersjs/authentication';
+import channelsModifyContextData from '../../hooks/channels-modify-context-data';
 // Don't remove this comment. It's needed to format import lines nicely.
 
 const { authenticate } = authentication.hooks;
@@ -8,7 +9,7 @@ export default {
     all: [ authenticate('jwt') ],
     find: [],
     get: [],
-    create: [],
+    create: [channelsModifyContextData()],
     update: [],
     patch: [],
     remove: []
