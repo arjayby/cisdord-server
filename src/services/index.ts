8e03bc9ef60fa117ddd931b88937497a3c3f09aa
Arjay Bayona <arjayby1@gmail.com>
2021-03-21 21:30:28 +0800

    ⚡ Add channels and messages service

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,11 @@
 import { Application } from '../declarations';
 import users from './users/users.service';
+import channels from './channels/channels.service';
+import messages from './messages/messages.service';
 // Don't remove this comment. It's needed to format import lines nicely.
 
 export default function (app: Application): void {
   app.configure(users);
+  app.configure(channels);
+  app.configure(messages);
 }
