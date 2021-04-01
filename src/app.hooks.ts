7080770f60bc9243af93c6b701c7efbd6b7d4687
Arjay Bayona <arjayby1@gmail.com>
2021-04-01 09:48:13 +0800

    🔐 Limit the rate to 100 requests every 2 minutes

--- src/app.hooks.ts
+++ src/app.hooks.ts
@@ -1,9 +1,22 @@
 // Application hooks that run for every service
 // Don't remove this comment. It's needed to format import lines nicely.
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { rateLimit } from 'feathers-fletching';
+import { RateLimiterMemory } from 'rate-limiter-flexible';
+
+// 100 requests every 2 minutes
+const rateLimiter = new RateLimiterMemory({
+  points: 100,
+  duration: 120
+});
+
+const rateLimitHook = rateLimit(rateLimiter);
+
 export default {
   before: {
-    all: [],
+    all: [rateLimitHook],
     find: [],
     get: [],
     create: [],
